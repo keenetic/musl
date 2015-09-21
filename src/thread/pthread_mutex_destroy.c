ea60098b5c62e2b693349da1b61e2aaf6da375b3
Eugene Yudin <e.yudin@ndmsystems.com>
2015-09-21 12:42:40 +0300

    Case 6007 (pthread_mutex_destroy): Add returning EBUSY.

--- src/thread/pthread_mutex_destroy.c
+++ src/thread/pthread_mutex_destroy.c
@@ -1,6 +1,13 @@
-#include <pthread.h>
+#include "pthread_impl.h"
 
-int pthread_mutex_destroy(pthread_mutex_t *mutex)
+int pthread_mutex_destroy(pthread_mutex_t *m)
 {
+	int type = m->_m_type & 3;
+
+	if (type == PTHREAD_MUTEX_NORMAL && m->_m_lock == EBUSY)
+		return EBUSY;
+	else if (type == PTHREAD_MUTEX_RECURSIVE && m->_m_count)
+		return EBUSY;
+
 	return 0;
 }
