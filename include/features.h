4a96f01c6c8cbd7fd119393ac59854191e982a25
Eugene Yudin <e.yudin@ndmsystems.com>
2020-09-09 17:16:26 +0300

    add headers from OpenWrt, NDMS-1021

--- include/features.h
+++ include/features.h
@@ -37,4 +37,20 @@
 
 #define __REDIR(x,y) __typeof__(x) x __asm__(#y)
 
+/* Convenience macros to test the versions of glibc and gcc.
+   Use them like this:
+   #if __GNUC_PREREQ (2,8)
+   ... code requiring gcc 2.8 or later ...
+   #endif
+   Note - they won't work for gcc1 or glibc1, since the _MINOR macros
+   were not defined then.  */
+#if defined __GNUC__ && defined __GNUC_MINOR__
+# define __GNUC_PREREQ(maj, min) \
+	((__GNUC__ << 16) + __GNUC_MINOR__ >= ((maj) << 16) + (min))
+#else
+# define __GNUC_PREREQ(maj, min) 0
+#endif
+
+#include <sys/glibc-types.h>
+
 #endif
