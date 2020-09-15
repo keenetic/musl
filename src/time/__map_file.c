f6a398461bfd43c3f7b81f1710e056cf973f74f9
Eugene Yudin <e.yudin@ndmsystems.com>
2020-09-15 17:13:46 +0300

    detect changes in timezone file, NDMS-1039

--- src/time/__map_file.c
+++ src/time/__map_file.c
@@ -3,7 +3,7 @@
 #include <sys/stat.h>
 #include "syscall.h"
 
-const char unsigned *__map_file(const char *pathname, size_t *size)
+const char unsigned *__map_file(const char *pathname, size_t *size, dev_t *dev, ino_t *ino, time_t *mtime)
 {
 	struct stat st;
 	const unsigned char *map = MAP_FAILED;
@@ -12,6 +12,9 @@ const char unsigned *__map_file(const char *pathname, size_t *size)
 	if (!__fstat(fd, &st)) {
 		map = __mmap(0, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
 		*size = st.st_size;
+		if (ino) *ino = st.st_ino;
+		if (dev) *dev = st.st_dev;
+		if (mtime) *mtime = st.st_mtim.tv_sec;
 	}
 	__syscall(SYS_close, fd);
 	return map == MAP_FAILED ? 0 : map;
