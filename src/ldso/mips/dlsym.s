1ac135ae065a922490c2d895ddb7c1bc853ee456
Sergey Korolev <s.korolev@ndmsystems.com>
2020-11-20 19:29:07 +0300

    mips: add CFI directives to assembler sources, NDMS-1223
    CFI directives moved to .debug_frame section (instead of .eh_frame)
    since these may be removed by a strip utility.

--- src/ldso/mips/dlsym.s
+++ src/ldso/mips/dlsym.s
@@ -1,17 +1,24 @@
+.cfi_sections .debug_frame
 .set noreorder
 .global dlsym
 .hidden __dlsym
 .type dlsym,@function
 dlsym:
+	.cfi_startproc
 	lui $gp, %hi(_gp_disp)
 	addiu $gp, %lo(_gp_disp)
 	addu $gp, $gp, $25
 	move $6, $ra
 	lw $25, %call16(__dlsym)($gp)
 	addiu $sp, $sp, -16
+	.cfi_adjust_cfa_offset 16
 	sw $ra, 12($sp)
+	.cfi_rel_offset $ra, 12
 	jalr $25
 	nop
 	lw $ra, 12($sp)
+	.cfi_restore $ra
 	jr $ra
 	addiu $sp, $sp, 16
+	.cfi_adjust_cfa_offset -16
+	.cfi_endproc
