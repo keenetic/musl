keenetic/musl

commit 54e05b22539841b7129dcc16d1eb23ef850bae87

drop direct use of stat syscalls in internal __map_file
this function is used to implement some baseline ISO C interfaces, so
it cannot call any of the stat functions by their public names. use
the namespace-safe __fstatat instead.

src/time/__map_file.c
@@ -1,16 +1,16 @@
+#define _BSD_SOURCE
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include "syscall.h"
-#include "kstat.h"
 
 const char unsigned *__map_file(const char *pathname, size_t *size, dev_t *dev, ino_t *ino, time_t *mtime)
 {
-	struct kstat st;
+	struct stat st;
 	const unsigned char *map = MAP_FAILED;
 	int fd = sys_open(pathname, O_RDONLY|O_CLOEXEC|O_NONBLOCK);
 	if (fd < 0) return 0;
-	if (!syscall(SYS_fstat, fd, &st)) {
+	if (!__fstatat(fd, "", &st, AT_EMPTY_PATH)) {
 		map = __mmap(0, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
 		*size = st.st_size;
 		if (ino) *ino = st.st_ino;

commit 89d2cc30e33a2277de72983ebbbf3a11049b521d

time: fix build of __map_file
Fix build regression after 54e05b22539841b7129dcc16d1eb23ef850bae87
("drop direct use of stat syscalls in internal __map_file")

src/time/__map_file.c
@@ -14,7 +14,7 @@ const char unsigned *__map_file(const char *pathname, size_t *size, dev_t *dev, 
 		*size = st.st_size;
 		if (ino) *ino = st.st_ino;
 		if (dev) *dev = st.st_dev;
-		if (mtime) *mtime = st.st_mtime_sec;
+		if (mtime) *mtime = st.st_mtim.tv_sec;
 	}
 	__syscall(SYS_close, fd);
 	return map == MAP_FAILED ? 0 : map;